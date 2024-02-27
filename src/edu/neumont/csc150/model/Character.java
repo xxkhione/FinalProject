5a08abf0c6e026b3258038009a26dffd30d4402d
Dami <68552016+xxkhione@users.noreply.github.com>
2024-02-27 11:18:04 -0700

    Updated Character.java

--- src/edu/neumont/csc150/model/Character.java
+++ src/edu/neumont/csc150/model/Character.java
@@ -7,5 +7,47 @@
 package edu.neumont.csc150.model;
 
 public abstract class Character {
+    public final static int MIN_HEALTH = 0;
+    private String name;
+    private Arsenal weapon;
+    private int damage;
+    private int health;
 
+
+
+    //region getters/setters
+    public String getName() {
+        return name;
+    }
+    private void setName(String name) {
+        if(name == null || name.isEmpty()){
+            throw new IllegalArgumentException("Character name cannot be empty or null.");
+        }
+        this.name = name;
+    }
+    public Arsenal getWeapon() {
+        return weapon;
+    }
+    private void setWeapon(Arsenal weapon) {
+        this.weapon = weapon;
+    }
+    public int getDamage() {
+        return damage;
+    }
+    public void setDamage(int damage) {
+        this.damage = damage;
+    }
+    public int getTotalDamage(int damage){
+        return damage; //TODO: need to implement the weapon damage once we get that method working
+    }
+    public int getHealth() {
+        return health;
+    }
+    protected abstract void setHealth(int health);
+    //endregion
+
+
+
+    @Override
+    public abstract String toString();
 }
