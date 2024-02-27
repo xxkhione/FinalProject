2fe8ae77d55fbb8b7316a9acc34c943335d3bc8a
eball0509 <161506233+eball0509@users.noreply.github.com>
2024-02-27 16:11:10 -0700

    Added all methods to the UI

--- src/edu/neumont/csc150/view/UI.java
+++ src/edu/neumont/csc150/view/UI.java
@@ -7,4 +7,50 @@
 package edu.neumont.csc150.view;
 
 public class UI {
+
+    public int mainMenu(){
+        return Console.getIntInput("""
+                1. New Game
+                2. Load Game
+                3. Exit""",1, 3);
+    }
+
+    public String getName(){
+        return Console.getStringInput("What is your name, warrior");
+        //comment
+    }
+
+    public void battleMenu(){
+
+    }
+
+    public void attackMenu(){
+
+    }
+
+    public int afterBattlePrompt(){
+        return Console.getIntInput("""
+                1. Save Game
+                2. Generate New Weapon
+                3. Quit""", 1, 3);
+    }
+
+    public int afterDeathPrompt(){
+        return Console.getIntInput("""
+                1.Load A Save
+                2.Exit""", 1, 2);
+    }
+
+    public void noSaves(){
+        Console.writeLn("You have no current saves");
+        //comment
+    }
+
+    public void allSaves(){
+        
+    }
+
+
+
+
 }
