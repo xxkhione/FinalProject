9516c9661d4acef0a5942343fdd27dd6d1392512
eball0509 <161506233+eball0509@users.noreply.github.com>
2024-02-29 10:59:20 -0700

    attack method for enemy

--- src/edu/neumont/csc150/model/Enemy.java
+++ src/edu/neumont/csc150/model/Enemy.java
@@ -6,19 +6,26 @@
  */
 package edu.neumont.csc150.model;
 
+import java.util.Random;
+
 public class Enemy extends Character{
 
 
 
     @Override
     public int takeDamage(int damage) {
-
+        int block = new Random().nextInt(50) + 1;
+        if (block == 19 ){
+            damage -= (int)(damage * 0.3f);
+        }
+        setHealth(getHealth() - damage, getMaxHealth());
+        return damage;
 
     }
 
     @Override
     public int attack(int damage) {
-
+        return 0;
     }
 
 
