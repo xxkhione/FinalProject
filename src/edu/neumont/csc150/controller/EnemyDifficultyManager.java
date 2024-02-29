d9eb58f966421d07409560c662ae44b4931b78b1
Dami <68552016+xxkhione@users.noreply.github.com>
2024-02-29 11:09:52 -0700

    Started EnemyDifficultyManager.java.

--- src/edu/neumont/csc150/controller/EnemyDifficultyManager.java
+++ src/edu/neumont/csc150/controller/EnemyDifficultyManager.java
@@ -6,6 +6,16 @@
  */
 package edu.neumont.csc150.controller;
 
-public class EnemyDifficultyManager {
+import edu.neumont.csc150.model.Game;
 
+public class EnemyDifficultyManager {
+    //Gets the trial number
+    //Based on the number (increments of 5)
+    //The damage & health of enemy get stronger
+    //Their chance of getting a harder hitting weapon increases
+    //Returns enemy with new properties
+
+    public static Character increaseDifficulty(Game game){
+        return null;
+    }
 }
