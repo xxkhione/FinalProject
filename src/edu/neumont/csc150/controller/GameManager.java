2301d4f6eeb0da35c6ee80b31c0dabe1199ab480
Dami <68552016+xxkhione@users.noreply.github.com>
2024-03-05 10:16:07 -0700

    GameManager.java JavaDocs.

--- src/edu/neumont/csc150/controller/GameManager.java
+++ src/edu/neumont/csc150/controller/GameManager.java
@@ -24,6 +24,11 @@ public class GameManager {
     }
     //endregion
 
+    /**
+     * The main loop of the game. If user selects one from the main menu, it starts a new game.
+     * 2 lists the saves and prompts them to pick a save to load.
+     * 3 exits the application.
+     */
     public void run(){
         ui.welcomeMessage();
         do {
@@ -43,6 +48,10 @@ public class GameManager {
         } while (true);
     }
 
+    /**
+     * If trial is equal to max trials (25) then it congratulates the user and brings them back to the main loop.
+     * Else, it prompts the user to save and continue, generate a new weapon, or save and exit.
+     */
     private void afterBattle(){
         if(game.getTrialNumber() == Game.MAX_TRIALS){
             ui.endGameMessage();
@@ -65,6 +74,9 @@ public class GameManager {
         }
     }
 
+    /**
+     * After the user dies in a trial, it prompts them to load a save, start a new game, or exit to main menu.
+     */
     private void afterDeath(){
         int selection = ui.afterDeathPrompt();
         switch (selection){
@@ -78,17 +90,28 @@ public class GameManager {
         }
     }
 
+    /**
+     * All logic to create a new game.
+     * Asks the user for their name, sets the instance of the game with the player information and starts the battle.
+     */
     private void newGame(){
         String name = ui.getName();
         Figure player = new Player(name);
         setGame(new Game(player));
         startBattle();
     }
+
+    /**
+     * Logic to save the current instance of the game.
+     */
     private void saveGame(){
         SaveManager.saveGame(getGame());
         ui.saveMessage();
     }
 
+    /**
+     * Loads up all current saves in the directory and prompts the user to pick a save to load.
+     */
     private void pickASave(){
         String saveName = ui.allSaves(SaveManager.getAllSaves());
         if(saveName == null){
@@ -98,6 +121,11 @@ public class GameManager {
         startBattle();
     }
 
+    /**
+     * Logic for starting a battle.
+     * Begins by regenerating player and enemy health, increases enemy difficulty (if applicable), and displays a battle view.
+     * Calls battle to initiate the fighting then checks if the player won.
+     */
     private void startBattle(){
         game.startOfNewTrial();
         game.setEnemy(EnemyDifficultyManager.increaseDifficulty(game, game.getTrialNumber()));
@@ -106,6 +134,11 @@ public class GameManager {
         checkIfPlayerWon(playerIsAlive);
     }
 
+    /**
+     * Logic of the actual battle.
+     * While both the enemy and player is alive, they take turns doing damage until one of them dies.
+     * @return playerIsAlive; if true, then the player won the trial, if false, the player died.
+     */
     private boolean battle(){
         boolean playersTurn = true;
         boolean playerIsAlive = game.getPlayer().isAlive(), enemyIsAlive = game.getEnemy().isAlive();
@@ -125,6 +158,11 @@ public class GameManager {
         }
         return playerIsAlive;
     }
+
+    /**
+     * Checks if the player won or lost and displayed a message based on the parameter.
+     * @param playerIsAlive - true or false based on if the player is alive or dead.
+     */
     private void checkIfPlayerWon(boolean playerIsAlive){
         if(playerIsAlive){
             ui.displayPlayerWonMessage(game.getTrialNumber());
